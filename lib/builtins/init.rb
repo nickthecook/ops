5504df49d5cbc4ccd1df401d55f95a57bce960de
nickthecook <nickthecook@gmail.com>
2020-04-12 10:48:16 -0400

    fix `ops init`

--- lib/builtins/init.rb
+++ lib/builtins/init.rb
@@ -7,7 +7,13 @@ require_relative "../builtin"
 module Builtins
 	class Init < Builtin
 		OPS_YML = "ops.yml"
-		OPS_YML_TEMPLATE = "etc/ops.template.yml"
+		OPS_YML_TEMPLATE = File.join(
+			File.dirname(__FILE__),
+			"..",
+			"..",
+			"etc",
+			"ops.template.yml"
+		)
 
 		def run
 			if File.exist?(OPS_YML)
