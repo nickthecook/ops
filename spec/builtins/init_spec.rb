89076ed1e398bbdefe560a15c7573c1cb73e134c
nickthecook <nickthecook@gmail.com>
2020-04-26 10:24:08 -0400

    add init_spec

---
+++ spec/builtins/init_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative "../../lib/builtins/init"
+
+RSpec.describe Builtins::Init do
+	subject { described_class.new(args, config) }
+
+	let(:args) { {} }
+	let(:config) { {} }
+
+	describe "#run" do
+		let(:result) { subject.run }
+		let(:ops_yml_exists) { false }
+		let(:ops_yml_template) { /\/etc\/ops.template.yml$/ }
+
+		before do
+			allow(File).to receive(:exist?).with("ops.yml").and_return(ops_yml_exists)
+		end
+
+		it "copies the template to ops.yml" do
+			expect(FileUtils).to receive(:cp).with(ops_yml_template, "ops.yml")
+			result
+		end
+
+		context "when ops.yml already exists" do
+			let(:ops_yml_exists) { true }
+
+			it "does not overwrite the existing file" do
+				expect(FileUtils).not_to receive(:cp)
+				result
+			end
+		end
+	end
+end
