85378e420e089790f89cb7a5719ad4a47d49f6ee
nickthecook <nickthecook@gmail.com>
2020-05-01 11:49:48 -0400

    don't try to load byebug

--- loader.rb
+++ loader.rb
@@ -2,6 +2,3 @@
 
 # for convenience, add "lib" to the load path
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/lib"))
-
-# if this is the dev env, load some debugging tools
-require 'pry-byebug' if ENV['environment'].nil? || ENV['environment'] == "development"
