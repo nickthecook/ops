5f32f053f2d8bdf1443f82b2404f3c3cd1ab66ce
nickthecook <nickthecook@gmail.com>
2020-08-28 09:45:50 -0400

    add some output to test container entrypoint

--- platforms/ops-debian/entrypoint.sh
+++ platforms/ops-debian/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # entrypoint for ops test containers
 
+echo "$0: loading SSH agent..."
 eval `ssh-agent`
+echo "$0: running 'bundler install'..."
 bundle install --quiet
+echo "$0: running 'ops up'..."
 bin/ops up
 
-echo "$*"
+echo "$0: Running command: $*"
 "$@"
